a234af3cbb0ecb30286dac43ae000bb5a9c17ff7
Charles Dong <2365354490@qq.com>
2017-03-08 20:35:36 +0800

    Create 2-4

---
+++ 2/4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Your age is equal to " << (age * 12) << " months\n";
+    return 0;
+}
