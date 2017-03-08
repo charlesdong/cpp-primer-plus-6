691e7ac2177fd40d9aba8a3e412fba4dd866aab2
Charles Dong <2365354490@qq.com>
2017-03-08 20:34:20 +0800

    Create 2-2

---
+++ 2/2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int l;
+    cout << "Enter the number of long(s): ";
+    cin >> l;
+    cout << l << " long(s) is " << (l * 220) << " miles.\n";
+    return 0;
+}
