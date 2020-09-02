d5acd2bf5fb16420ec88f0f4899143156fa36c5d
Nicolò Steffè <nicolo.steffe@gmail.com>
2020-09-02 16:51:17 +0200

    Update 6.cpp
    I would write it like this because on the exercise it's requested to "Write a program that has main() call a user-defined function" 👍

--- 2/6.cpp
+++ 2/6.cpp
@@ -1,14 +1,20 @@
+
 #include <iostream>
 
 double astro(double a);
 
-int main()
+void conversion()
 {
     using namespace std;
     double light_years;
     cout << "Enter the number of light years: ";
     cin >> light_years;
     cout << light_years << " light years = " << astro(light_years) << " astronomical units.\n";
+}
+
+int main()
+{
+    conversion();
     return 0;
 }
 
